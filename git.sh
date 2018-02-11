5dfc68fc8c60983689925b89e04a1b9eeecb10e0
Hosszulepes <hacka95@gmail.com>
2018-02-11 14:02:30 +0100

    elso push

---
+++ git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get update
+apt-get -f install -y
+apt-get install git -y
---
+++ google.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install -y libgconf2-4 libnss3-1d libxss1
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+rm -f google-chrome-stable_current_amd64.deb
