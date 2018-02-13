ec9cc3d5add944d132886751fb38d49854f88851
pocandraspoc <pocandraspoc@gmail.com>
2018-02-13 18:33:22 +0100

    changed order
    If yo could already install sublime text with apt-get install
    then you would not need to add reposisoty key and download the package from the repo

--- subl.sh
+++ subl.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get upgrade -y
-apt-get install sublime-text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+apt-get install sublime-text -y
 rm -f sublimehq-pub.gpg
