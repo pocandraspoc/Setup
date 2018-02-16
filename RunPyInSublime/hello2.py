fe063534aa955c8bfeed94b7f5fbc1de77db4d4d
pocandraspoc <pocandraspoc@gmail.com>
2018-02-16 12:25:51 +0100

    Setup sublime

--- RunPyInSublime/hello2.py
+++ RunPyInSublime/hello2.py
@@ -7,6 +7,9 @@ print("By default ")
 print("if you press 'Ctrl + Shift + B' you will be able to select")
 print("Python 'builder' and run your code")
 
+print("consider to use")
+print("https://realpython.com/blog/python/setting-up-sublime-text-3-for-full-stack-python-development/")
+
 #in subline menu go
 #tools >> Build systems >> new build sys
 #and put text into the file and save it with a
