96ba0085049b9688574fe0b1b0805b7a8d36279e
pocandraspoc <pocandraspoc@gmail.com>
2018-02-16 10:18:20 +0100

    set up sublime for py

---
+++ RunPyInSublime/hello2.py
@@ -0,0 +1,21 @@
+import sys
+
+print(sys.version)
+
+print("hello")
+print("By default ")
+print("if you press 'Ctrl + Shift + B' you will be able to select")
+print("Python 'builder' and run your code")
+
+#in subline menu go
+#tools >> Build systems >> new build sys
+#and put text into the file and save it with a
+#meaningful name
+
+#{
+#    "cmd": ["python3", "-i", "-u", "$file"],
+#    "file_regex": "^[ ]File \"(...?)\", line ([0-9]*)",
+#    "selector": "source.python"
+#}
+
+
